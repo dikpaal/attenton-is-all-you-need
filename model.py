f0fb1b415f16c88f3f423e888a777853215118ab
dikpaal <dikpaal@icloud.com>
2025-03-08 15:15:57 -0800

    done MHA

--- model.py
+++ model.py
@@ -67,14 +67,45 @@ class LayerNormalization(nn.Module):
     
 class FeedForwardBlock(nn.Module):
 
-    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+    def __init__(self, d: int, d_ff: int, dropout: float) -> None:
         
         super().__init__()
-        self.linear_1 = nn.Linear(d_model, d_ff) # w1, b1
+        self.linear_1 = nn.Linear(d, d_ff) # w1, b1
         self.dropout = nn.Dropout(dropout)
-        self.linear_2 = nn.Linear(d_ff, d_model) # w2, b2
+        self.linear_2 = nn.Linear(d_ff, d) # w2, b2
 
     def forward(self, x):
         
-        # (batch, sequence_len, d_model) --> (batch, sequence_len, d_ff) --> (batch, sequence_len, d_model)
+        # (batch, sequence_len, d) --> (batch, sequence_len, d_ff) --> (batch, sequence_len, d)
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d = d
+        self.h = h
+        assert d % h == 0, "d is not divisible by h" # d should be divisible by h
+
+        self.d_k = d // h # Dimension of vector seen by each head
+        self.w_q = nn.Linear(d, d, bias=False)
+        self.w_k = nn.Linear(d, d, bias=False)
+        self.w_v = nn.Linear(d, d, bias=False)
+        self.w_o = nn.Linear(d, d, bias=False)
+        
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) 
+        key = self.w_k(k) 
+        value = self.w_v(v)
+
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+        
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        return self.w_o(x)
