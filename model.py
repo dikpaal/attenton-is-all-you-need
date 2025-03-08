48499460325e99a4d4a101bd2edfcc8d9f488fdd
dikpaal <dikpaal@icloud.com>
2025-03-08 13:30:12 -0800

    FIXED DUMB BUGS IN LAYER NORM

--- model.py
+++ model.py
@@ -46,15 +46,15 @@ class PositionalEncoding(nn.Module):
     
 class LayerNormalization(nn.Module):
 
-    def __init__(self, features: int, eps: float = 10 ** 6) -> None:
-        
+    def __init__(self, features: int, eps: float = 10 **- 6) -> None:
+
         super().__init__()
         self.eps = eps
         self.alpha = nn.Parameter(torch.ones(features))
         self.bias = nn.Parameter(torch.zeros(features))
 
     def forward(self, x):
-        
+
         mean = x.mean(dim = -1, keepdim = True)
-        std = x.std(dim = 1, keepdim = True)
-        return self.alpha * (x + mean) / (std - self.eps) + self.bias
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
